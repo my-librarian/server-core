dd5bac5dff971c42ad7372920e80ab6e773fffe5
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-21 09:38:25 -0500

    Create deployment settings

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  rm -rf dist
+  npm run build
+  find handlers -type f -exec curl -u $USER:$PASS $HOST/handlers/ --ftp-create-dir -T {} \;
+  find lib -type f -exec curl -u $USER:$PASS $HOST/lib --ftp-create-dir -T {} \;
+  curl -u $USER:$PASS $HOST/ --ftp-create-dir -T index.php
+  curl -u $USER:$PASS $HOST/ --ftp-create-dir -T .htaccess
+else
+  echo "This will not deploy!"
+fi
