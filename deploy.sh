f06a910d37c09a529ab689223aab82581b9c5012
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-21 12:46:06 -0500

    v0.2.1

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   find handlers -type f -exec curl -u $USER:$PASS $HOST/handlers/ --ftp-create-dir -T {} \;
-  find lib -type f -exec curl -u $USER:$PASS $HOST/lib --ftp-create-dir -T {} \;
+  find lib -type f -exec curl -u $USER:$PASS $HOST/lib/ --ftp-create-dir -T {} \;
   curl -u $USER:$PASS $HOST/ --ftp-create-dir -T index.php
   curl -u $USER:$PASS $HOST/ --ftp-create-dir -T .htaccess
 else
