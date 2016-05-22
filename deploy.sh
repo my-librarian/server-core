1e61501f04aa0f63702c932334693e5d1949fada
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-22 08:28:43 -0500

    Update deployment environment before publishing

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  echo env=prod > lib/deploy.ini
   find handlers -type f -exec curl -u $USER:$PASS $HOST/handlers/ --ftp-create-dir -T {} \;
   find lib -type f -exec curl -u $USER:$PASS $HOST/lib/ --ftp-create-dir -T {} \;
   curl -u $USER:$PASS $HOST/ --ftp-create-dir -T index.php
