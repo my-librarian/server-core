5ef7b03ef81766fb92c34195370efee3b1c79d44
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-28 07:30:40 -0500

    Test user credentials

--- handlers/user.php
+++ handlers/user.php
@@ -2,17 +2,37 @@
 
 namespace handlers;
 
+use lib\Error;
 use lib\Handler;
 
 class User extends Handler {
 
+    private function encrypt($plain) {
+
+        return md5($plain);
+    }
+
     function post($data) {
 
         $columns = ['name', 'deptno', 'password'];
-        $values = [$data['name'], $data['deptNo'], md5($data['pass'])];
+        $values = [$data['name'], $data['deptNo'], $this->encrypt($data['pass'])];
 
         $id = $this->insert('users', $columns, $values);
 
         $this->send(['id' => $id]);
+    }
+
+    function get($deptNo, $pass) {
+
+        $ids = $this->select(
+            'SELECT userid FROM users WHERE deptno=? AND password=?',
+            [$deptNo, $this->encrypt($pass)]
+        );
+
+        if(count($ids) > 0) {
+            $this->send();
+        } else {
+            (new Error('Invalid Credentials', 401))->send();
+        }
     }
 }
