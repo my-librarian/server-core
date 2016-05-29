00331917bf46f079b0da9ead9efade79bc9459dd
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-29 10:28:53 -0500

    Add authors to books response

--- handlers/book.php
+++ handlers/book.php
@@ -2,6 +2,7 @@
 
 namespace handlers;
 
+use lib\Error;
 use lib\Handler;
 
 class Book extends Handler {
@@ -9,10 +10,25 @@ class Book extends Handler {
     function get($id) {
 
         $result = $this->select(
-            'SELECT * FROM books WHERE bookid = ?',
+            'SELECT *, subjects.name AS subject FROM books ' .
+            'JOIN subjects USING(subjectid) ' .
+            'WHERE bookid = ?',
             [$id]
         );
 
-        $this->send($result[0]);
+        if (count($result) < 1) {
+            (new Error('Subject Not Found'))->send();
+            exit();
+        }
+
+        $result = $result[0];
+
+        $result['authors'] = $this->select(
+            'SELECT authorid, name FROM authors '.
+            'JOIN authorassoc USING(authorid) WHERE bookid = ?',
+            [$id]
+        );
+
+        $this->send($result);
     }
 }
