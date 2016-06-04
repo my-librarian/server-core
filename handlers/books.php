26e599ab67cc8aa1e480d1ca46d58fdb6b906996
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-06-04 13:36:35 -0500

    Get books filters
    Updated get books list endpoint

--- handlers/books.php
+++ handlers/books.php
@@ -2,14 +2,56 @@
 
 namespace handlers;
 
+use lib\Error;
 use lib\Handler;
 
 class Books extends Handler {
 
-    function get() {
+    private function listFilters() {
 
-        $result = $this->select('select * from books');
+        $response = [];
+
+        $response['authors'] = (new Authors())->getAuthorsChecklist();
+        $response['subjects'] = (new Subjects())->getSubjectsChecklist();
+        $response['racks'] = array_map(
+            function ($index) {
+
+                return [
+                    'label' => $index,
+                    'selected' => FALSE
+                ];
+            },
+            range(1, 17)
+        );
+        $response['borrowed'] = FALSE;
+        $response['available'] = FALSE;
+
+        $this->send($response);
+    }
+
+    private function listBooks() {
+
+        $result = $this->select('SELECT * FROM books');
 
         $this->send($result);
+    }
+
+    function get($command) {
+
+        $commands = [
+            'list' => 'listBooks',
+            'filters' => 'listFilters'
+        ];
+
+        if (method_exists($this, $commands[$command])) {
+            $this->$commands[$command]();
+        } else {
+            (new Error('API Not Implemented', 405))->send();
+        }
+    }
+
+    function put($filters) {
+
+        $this->listBooks();
     }
 }
