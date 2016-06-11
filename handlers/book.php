d2748e40a462ae79df7f55df05f1bd0dd9a09622
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-06-11 13:07:31 -0500

    Create book

--- handlers/book.php
+++ handlers/book.php
@@ -7,6 +7,35 @@ use lib\Handler;
 
 class Book extends Handler {
 
+    private function formatDate($dateTimeString) {
+
+        return date('Y-m-d', strtotime($dateTimeString));
+    }
+
+    private function insertAuthors($data, $bookid) {
+
+        foreach ($data['authors'] as $author) {
+            $authorid = (new Author())->insertAuthor($author['name']);
+            $this->insert('authorassoc', ['bookid', 'authorid'], [$bookid, $authorid]);
+        }
+    }
+
+    private function insertBook($data) {
+
+        $data['adddate'] = $this->formatDate($data['adddate']);
+
+        $columns = ['title', 'accessno', 'adddate', 'rackno', 'subjectid'];
+        $values = array_map(
+            function ($column) use ($data) {
+
+                return $data[$column];
+            },
+            $columns
+        );
+
+        return $this->insert('books', $columns, $values);
+    }
+
     function get($id) {
 
         $result = $this->select(
@@ -32,6 +61,23 @@ class Book extends Handler {
         $this->send($result);
     }
 
-    function post($book) {
+    function post($data) {
+
+        $this->mysqli->autocommit(FALSE);
+
+        $data['subjectid'] = (new Subject())->insertSubject($data['subject']);
+
+        $bookid = $this->insertBook($data);
+
+        $this->insertAuthors($data, $bookid);
+
+        if ($this->mysqli->errno) {
+            $_500 = new Error($this->mysqli->error, 500);
+            $_500->send();
+        } else {
+            $this->mysqli->commit();
+        }
+
+        $this->send(['id' => $bookid]);
     }
 }
