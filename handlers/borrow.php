3d1f413d29a9300633929092960d258547cec8c2
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-06-19 13:02:23 -0500

    Borrow books

---
+++ handlers/borrow.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace handlers;
+
+use lib\Handler;
+
+class Borrow extends Handler {
+
+    function get($bookid) {
+
+        $response = $this->select(
+            'SELECT b.name AS borrowerName, i.name AS issuerName, borrow.* FROM borrow ' .
+            'JOIN users b USING(userid) ' .
+            'JOIN users i ON issuerid = i.userid ' .
+            'WHERE bookid = ? '.
+            'ORDER BY borrowdate DESC',
+            [$bookid]
+        );
+
+        $this->send($response);
+    }
+
+    function getDueDate($timespan) {
+
+        if ($timespan === 'short') {
+            return date('Y-m-d', strtotime('+4 weeks'));
+        }
+
+        return date('Y-m-d', strtotime('+12 weeks'));
+    }
+
+    function post($data) {
+
+        $values = [
+            (new User())->getUserIdFromDeptNo($data['userid']),
+            (new User())->getUserIdFromDeptNo($data['issuerid']),
+            $data['bookid'],
+            $this->getDueDate($data['timespan'])
+        ];
+
+        $id = $this->insert('borrow', ['userid', 'issuerid', 'bookid', 'duedate'], $values);
+
+        $this->send(['id' => $id]);
+    }
+}
