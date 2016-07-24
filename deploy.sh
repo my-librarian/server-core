d8df8aa55c1747596cf4fb9db43f6d7c6e127cb1
Travis CI <travis@localhost.localdomain>
2016-07-24 09:16:26 -0500

    Deploy to openshift

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 
-if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  echo env=prod > lib/deploy.ini
-  find handlers -type f -exec curl -u $USER:$PASS $HOST/handlers/ --ftp-create-dir -T {} \;
-  find lib -type f -exec curl -u $USER:$PASS $HOST/lib/ --ftp-create-dir -T {} \;
-  curl -u $USER:$PASS $HOST/ --ftp-create-dir -T index.php
-  curl -u $USER:$PASS $HOST/ --ftp-create-dir -T .htaccess
-  curl -u $USER:$PASS $HOST/ --ftp-create-dir -T version.ini
-else
-  echo "This will not deploy!"
-fi
+file_deploy() {
+    if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+      echo env=prod > lib/deploy.ini
+      find handlers -type f -exec curl -u $USER:$PASS $HOST/handlers/ --ftp-create-dir -T {} \;
+      find lib -type f -exec curl -u $USER:$PASS $HOST/lib/ --ftp-create-dir -T {} \;
+      curl -u $USER:$PASS $HOST/ --ftp-create-dir -T index.php
+      curl -u $USER:$PASS $HOST/ --ftp-create-dir -T .htaccess
+      curl -u $USER:$PASS $HOST/ --ftp-create-dir -T version.ini
+    else
+      echo "This will not deploy!"
+    fi
+}
+
+os_deploy() {
+    mkdir -p api
+    mv handlers api/
+    mv lib api/
+    mv index.php api/
+    mv .htaccess api/
+    sh -c "ls -a | grep -v -E 'api|deploy.sh' | xargs rm -rf || true"
+    git init
+    echo "$TRAVIS_TAG" > version.txt
+    git config --global user.email "vipranarayan14@gmail.com"
+    git config --global user.name "Travis CI"
+    git add --all
+    git commit -am "Deploy version $TRAVIS_TAG"
+}
