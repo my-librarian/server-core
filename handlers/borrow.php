1a64e4da976b5e671e73b2d2c6a3c8be257a9d82
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-09-18 10:00:25 -0500

    Get borrowed books for profiling

--- handlers/borrow.php
+++ handlers/borrow.php
@@ -66,4 +66,28 @@ class Borrow extends Handler {
             $borrowid
         );
     }
+
+    function getBorrowedBooks($userid) {
+
+        return $this->select(
+            'SELECT title, borrowdate, bookid, duedate FROM borrow ' .
+            'JOIN books USING (bookid) ' .
+            'WHERE userid = ? AND returndate IS NULL',
+            [$userid]
+        );
+    }
+
+    function getBorrowHistory($userid) {
+
+        return $this->select(
+            'SELECT b.name borrowerName, i.name issuerName, r.name receiverName, borrow.*, title bookTitle FROM borrow ' .
+            'JOIN users b USING(userid) ' .
+            'JOIN users i ON issuerid = i.userid ' .
+            'JOIN users r ON receiverid = r.userid ' .
+            'JOIN books USING(bookid) ' .
+            'WHERE borrow.userid = ? ' .
+            'ORDER BY borrowdate DESC',
+            [$userid]
+        );
+    }
 }
